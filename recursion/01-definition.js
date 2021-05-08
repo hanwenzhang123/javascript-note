5a8e08a42c1424a57ad0f5feb6a7e2bb0d4dbc93
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-08 16:06:59 -0400

    Create 01-definition.js

---
+++ recursion/01-definition.js
@@ -0,0 +1,10 @@
+recursion occurs when a function calls itself
+
+recursion is an altrenative to iteraion (using a loop)
+
+in the real world, you may see recursion instead of iteration when a recursive solution is
+- easier to read about (recursion helps break big problems into small chunks
+- easier to read than an iterative solution
+- won't negatively affect performance too much (Recursion uses more memory but is sometimes clearer and more readable.)
+
+  

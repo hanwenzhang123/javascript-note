0f9125960982c11438b02c7d0475ec30e06c53f9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-08 17:53:37 -0400

    Update 01-definition.js

--- recursion/01-definition.js
+++ recursion/01-definition.js
@@ -5,6 +5,7 @@ recursion is an altrenative to iteraion (using a loop)
 in the real world, you may see recursion instead of iteration when a recursive solution is
 - easier to read about (recursion helps break big problems into small chunks
 - easier to read than an iterative solution
-- won't negatively affect performance too much (Recursion uses more memory but is sometimes clearer and more readable.)
+- won't negatively affect performance too much, recursion needs a lot memory
+(Recursion uses more memory but is sometimes clearer and more readable.)
 
   

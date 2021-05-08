da561a40977e0794f7c94c51ef8d411214accead
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-07 23:01:48 -0400

    Create 05-this-keyword.js

---
+++ object/05-this-keyword.js
@@ -0,0 +1,40 @@
+// introduction to the keyword 'this'
+// like in English, me to refer our own name, in object, it will be the keyword 'this'
+
+// this - a magical keyword refers to the entire object itself (the container itself)
+
+let graceHopper = {
+  first: 'Grace',
+  last: 'Hopper',
+  rank: 'Rear Amiral'
+  sayQuote: function() {
+    console.log('a quote here');
+  },
+  getAge: function(year) {
+    return year - 1906; 
+  },
+  greet: function(name) {
+    console.log('hello, ' + name + '! My name is ' + this.first + ' ' this.last);  // refers to the entire object
+  }  
+};
+
+
+let calc = {
+  total: 0,
+  plus: function(num) {     //add a number to the total
+    this.total += num;
+    return this.total;
+  }
+  minus: function(num) {     //minus a number from the total
+    this.total -= num;
+    return this.total;
+  }
+};
+
+calc.plus(5);
+calc.plus(5);
+console.log(calc.total);
+
+
+
+    

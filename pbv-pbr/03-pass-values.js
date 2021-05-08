7650eec5bd6de8d59481235d52c52acf701b8510
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-08 11:13:36 -0400

    Create 03-pass-values.js

---
+++ pbv-pbr/03-pass-values.js
@@ -0,0 +1,31 @@
+passing values into functions
+
+//passing primitive values
+let myNum = 10;
+
+function adds2(num) {
+  num += 20;
+  return num;
+}
+
+let returnedNum = adds20(myNum);
+
+console.log(myNum);   //10
+console.log(returnedNum);   //30
+
+//passing complex values
+let myArray = [10];
+
+function pushes20(array) {
+  arrray.push(20);
+  return array;
+}
+
+let returnedArray = pushes20(myArray);
+
+console.log(myArray);   //10, 20
+console.log(returnedArray);   //10, 20
+
+
+
+  

0cf9300abdfe74ce419ac1c39cdde6bcbdfc9d9e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-08 12:12:17 -0400

    Update 04-equality-operator.js

--- pbv-pbr/04-equality-operator.js
+++ pbv-pbr/04-equality-operator.js
@@ -2,10 +2,10 @@ equality operators and PBV/PBR
 
 === will compare complex values by reference, not by value!
   
-let array1 = [1, 2, 3];
-let array2 = [1, 2, 3];
+let array1 = [1, 2, 3];   //0*01
+let array2 = [1, 2, 3];   //0*02
 
-console.log ( array1 === array2 )   //False - because they both reference to different reference/memory even though they look same
+console.log ( array1 === array2 )   //False - because they both reference to two different memories even though they look same by value.
 
 
 

ca2eacf9fed525c5b21677bb3c777b96183786a9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-08 12:35:42 -0400

    Create 01-basics.js

---
+++ function/01-basics.js
@@ -0,0 +1,12 @@
+why are functions special?
+functions ara special in JS because they are not speccial
+we think of function as being different from other values in JS
+strings, numbers, arrays: we are used to passing them into functions or returnining them from functions
+but function sometimes seem like they aarre in a differrent category, rooted to the line of code where they are defined
+in JS, function are first-class objects, where is another way of saying that functions are like any other value in JS
+
+
+we can use function as variables in and out
+function is a collection of instructions they are essentiaally some capability and functionality that packed together use again and again
+in JS, function can also give another function to use that funtion inside another function again and again
+

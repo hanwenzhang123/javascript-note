0b5dd0064681d67bc52cca2dc8007d45599df579
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-08 10:54:46 -0400

    Update 02-variable-assignment.js

--- pbv-pbr/02-variable-assignment.js
+++ pbv-pbr/02-variable-assignment.js
@@ -13,4 +13,40 @@ console.log(second);    //21
 
 //complex
 when you assign a complex value to a variable, the variable DOES NOT store a copy of the value
-Instead, the variable stores a reference 
+Instead, the variable stores a reference in memory; the reference points to the value
+
+//situation 1
+let first = [10, 20]; //first stores a reference to [10, 20]
+first.push(30);   //first still stores the same reference
+
+console.log(first);   //10, 20, 30
+
+//situation 2
+let first = [10, 20];
+let second = [10, 20];    //second stores a new reference to a new array
+
+second.push(30);   //since first and second reference different arrays, pushing a value into second will not affect the array referenced by first
+
+console.log(first);   //10, 20
+console.log(second);    //10, 20, 30
+
+//situation 3
+let first = [10, 20];
+let second = first;  //second now stores a copy of the REFERENCE that was originally stored in first
+
+seccond.push(30); //first and second share the same reference to the same array
+
+console.log(first);   //10, 20, 30
+console.log(second);    //10, 20, 30
+
+//situation 4
+let first = [10, 20];
+let second = first.slice(); //slice creates a new array, slice() creates a copy, not pass any value otherwise creates a copy of that portion
+
+seccond.push(30);  //first and second reference different arrays
+
+console.log(first);   //10, 20
+console.log(second);    //10, 20, 30
+
+
+

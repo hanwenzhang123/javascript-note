f56773b1b96f13488a028a5eef0fd9fb5e85d99f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-08 12:28:38 -0400

    Delete javascript directory

--- javascript/function-default.js
+++
@@ -1,8 +0,0 @@
-function getArea(width, length, unit) {
-  const area = width * length;
-  return `${area} ${unit}`;
-}
-
-function sayGreeting(greeting = 'Good morning', name= 'student'{
-  return `${greeting} ${student}`;
-}
--- javascript/function-isNAN.js
+++
@@ -1,4 +0,0 @@
-function getRandomNumber(lower, upper = 100) {
-if( isNaN(lower) || isNaN(upper) ) {
- throw Error('both arguments must be numbers.'); }
- return Math.floor(math.random() * ( upper - lower + 1) ) + lower; }

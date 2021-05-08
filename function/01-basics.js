6eab519ee79795b8ecd35dd5b05be8731bef382f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-08 12:58:34 -0400

    Update 01-basics.js

--- function/01-basics.js
+++ function/01-basics.js
@@ -11,7 +11,6 @@ function is a collection of instructions they are essentiaally some capability a
 in JS, function can also give another function to use that funtion inside another function again and again
 
 
-
 // we know we can push strings, or any value into arrays
 
 let amazingArray = []
@@ -23,6 +22,7 @@ amazingArray.push(happyString);
 amazingArray.push(happyString);
 
 console.log(amazingArray);    //[happy haappy happyy]
+
 
 
 //functions are not special. we can push them into an array, too
@@ -40,4 +40,35 @@ amazingArray.push(happyString);
 console.log(amazingArray);    //[[Function], [Function], [Function]]
 
 
-  
+
+// Anonymous Function
+function happyFunction() {
+  console.log('I am happy!');
+}
+
+let happyFunction = function() {    //anonymous function like having a recipe without name, here assign it to name happyFunction
+  console.log('I am happy!');
+};
+
+happyFunction()   // I am happy! for both functions above
+
+
+
+
+//how do we call all the functions in the array? how have we always looped through an array of values?
+
+function happyFunction() {
+  console.log('I am happy!');
+};
+
+let amazingArray = [happyFunction, happyFunction, happyFunction];
+
+for(let i = 0; i< amazingArray.length; i++) {
+  let element = amazingArray[i];        // each element is a function
+  element();
+}
+//I am happy!
+//I am happy!
+//I am happy!
+
+

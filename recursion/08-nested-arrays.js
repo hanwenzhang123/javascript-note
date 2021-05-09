e1f538dc45a2e18bd31608b83211e11bcdcee7be
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-08 21:34:37 -0400

    Update 08-nested-arrays.js

--- recursion/08-nested-arrays.js
+++ recursion/08-nested-arrays.js
@@ -1,1 +1,38 @@
+concat elements - Concatenate like join to the end of
+Concatenate, concatenation, or concat is a term that describes combining a string, text, or other data in a series without any gaps. 
 
+//example 1
+function concatEls(array) {
+  let finalSring = '';
+  for (let i = 0; i < array.length; i++) {
+    let element = array[i];
+    finalString += element;
+  }
+  return finalString;
+}
+
+let result = concatEls(['a', 'b', 'c']);
+console.log(result);
+//abc
+
+
+//example 2
+function concatEls(array) {
+  let finalSring = '';
+  for (let i = 0; i < array.length; i++) {
+    let element = array[i];
+    if (Array.isArray(element)) {
+      finalString += concatEls(element);
+    } else {
+    finalString += element;
+  }
+  return finalString;
+}
+
+let result = concatEls(['a', ['b', 'c']]);
+console.log(result);
+//abc
+  
+  
+  
+  

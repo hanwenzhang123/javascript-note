5c97bfcc7446f60bc76a9147632d5a8d170d36e9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-13 21:40:21 -0400

    Update 01-prototype.js

--- oop-js/01-prototype.js
+++ oop-js/01-prototype.js
@@ -13,5 +13,44 @@ Array.prototype.pop = function() {
 const nums = [ 6, 7, 8, 9 ];
 nums.pop(); // "SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!"
 
+
  
 __proto_(dunder proto): is a property that reference the prototype
+
+Array.prototype - all the method list under the __proto__, array will have access to. like concat, includes, etc
+
+
+All JavaScript objects inherit properties and methods from a prototype:
+
+Date objects inherit from Date.prototype
+Array objects inherit from Array.prototype
+Person objects inherit from Person.prototype
+
+
+Using the prototype Property
+The JavaScript prototype property allows you to add new properties to object constructors:
+
+Example
+function Person(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+}
+
+Person.prototype.nationality = "English";
+
+
+The JavaScript prototype property also allows you to add new methods to objects constructors:
+
+Example
+function Person(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+}
+
+Person.prototype.name = function() {
+  return this.firstName + " " + this.lastName;
+};

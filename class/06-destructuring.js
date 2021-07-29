3b3717f847c48a91bfb193ead7f1db86d9e8ffb6
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-29 18:53:32 -0400

    Update 06-destructuring.js

--- class/06-destructuring.js
+++ class/06-destructuring.js
@@ -1,15 +1,28 @@
 Destructuring
 - easily extract array element or object properties and store them in variables
+- where you use curly braces on the left and target a property with its property
 
-Versus Spread
+//Versus Spread
 - spread operators takes all elements, all properties
+
 
 //Array Destructuring
 [a, b] = ["Hello", "Max"]
 console.log(a) //Hello
 console.log(b) //Max
 
+const numbers = [1, 2, 3];
+[num1, num2] = numbers;
+console.log(num1, num2);    //1, 2
+
+const numbers = [1, 2, 3];
+[num1, , num3] = numbers;
+console.log(num1, num3);    //1, 3
+
+
 //Object Destructuring
 {name} = {name: "Max", age:28}
 console.log(name) //Max
-console.log(age)  //undefined. 
+console.log(age)  //undefined
+
+  

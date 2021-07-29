9990c085833e51d24a43bcbf713f04f51d67a29e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-29 13:47:34 -0400

    Create 08-array-functions.js

---
+++ class/08-array-functions.js
@@ -0,0 +1,10 @@
+// .map() is a built-in method
+
+const numbers = [1, 2, 3];
+
+const doubleNumArray = numbers.map((num) => {
+  return num * 2;
+});
+
+console.log(numbers);
+console.log(doubleNumArray);

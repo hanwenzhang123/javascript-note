c631507f80ad010b7c5b142c9fb59eac29e6ea3f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-29 19:20:27 -0400

    Update 08-array-functions.js

--- class/08-array-functions.js
+++ class/08-array-functions.js
@@ -2,9 +2,10 @@
 
 const numbers = [1, 2, 3];
 
-const doubleNumArray = numbers.map((num) => {
-  return num * 2;
-});
+const doubleNumArray = numbers.map((num) => {   //like map(), a built-in js array method, that executed on each element in the array here.
+  return num * 2;   //store the value to a new variable
+}); 
 
-console.log(numbers);
-console.log(doubleNumArray);
+console.log(numbers); //[1, 2, 3] the old one is unchanged
+console.log(doubleNumArray);  //[2, 4, 6] the new one holds double the values
+  

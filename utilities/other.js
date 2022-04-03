b231d428a7fbf82ec34a8a66e8cb65ed37250b84
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-04-02 22:10:18 -0400

    Update other.js

--- utilities/other.js
+++ utilities/other.js
@@ -20,3 +20,49 @@ export function memoize(cb) {   //saving data and use it later on
     return result
   }
 }
+
+
+//Memoization
+//You store the previous result - cache the value based on the input
+//get the same value over and over again
+//fetch external resources to save the call to server from stored the result
+
+const prevValues = [];
+
+function square(n) {
+  if (prevValues[n] != null){
+    return prevValues[n];
+  }
+  
+  let result = 0;
+  for(let i = 1; i <= n; i++){
+    for(let i = 1; i <= n; i++){
+      return += 1
+    }
+  }
+  
+  prevValues[n] = result;
+  return result;
+}
+
+//Dynamic Programming
+//take recursive function that calls itself multiple times with the same input
+//you memorize the same inputs
+
+function fib(n, prevValues = []) {
+  if (prevValues[n] != null) {
+    return prevValues[n]
+  }
+  let result;
+  if (n <= 2) {
+    result = 1;
+  } else {
+    result = fib(n - 1) + fib (n - 2);
+  }
+  prevValues[n] = result;
+  return result;
+}
+
+console.log(fib(40))
+
+
